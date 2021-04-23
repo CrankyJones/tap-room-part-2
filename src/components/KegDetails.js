79aba0e5aa19a8388634d2efa6cb933374ac646a
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-04-23 15:21:27 -0700

    Fix README diagram, Add color change on quantity for KegDetails

--- src/components/KegDetails.js
+++ src/components/KegDetails.js
@@ -18,21 +18,68 @@ function KegDetails(props){
     marginLeft: '8%',
     borderRadius:'8px',
   }
+  const styleQuarterLeft = {
+    backgroundColor: "orange",
+    color: "white",    
+    textAlign: 'left',
+    paddingTop: '2%',
+    paddingBottom: '2%',
+    paddingRight: '2%',
+    paddingLeft: '2%',
+    marginRight: '70%',
+    marginLeft: '8%',
+    borderRadius:'8px',
+      }
+  const styleTenLeft = {
+    backgroundColor: "red",
+    color: "white",    
+    textAlign: 'left',
+    paddingTop: '2%',
+    paddingBottom: '2%',
+    paddingRight: '2%',
+    paddingLeft: '2%',
+    marginRight: '70%',
+    marginLeft: '8%',
+    borderRadius:'8px',
+      }
+  
   return (
     <>
       <h2 style={styleKegInfo}>Keg Info</h2>
-    <div style={styleListItems}>
-      <h3>{keg.name}</h3>
-      <p>Price per Pint: {keg.price}</p>
-      <p>Alcohol Content {keg.potency}% ABV</p>
-      <p>{keg.description}</p>
-      <p>Current Pints Remaining: {keg.quantity}</p>
-      {keg.quantity > 0 &&
-        <button className='button' onClick={()=> onServingPint(keg.id) }>Serve Pint</button>}
-      {keg.quantity === 0 &&
-        <h1>Keg is Cashed. Waiting for some fresh brew.</h1> &&
-        <button className='kegButton' onClick={()=> onSwappingKeg(keg.id) }>Swap Keg</button>}
-    </div>
+
+      {keg.quantity > 25 &&
+        <div style = {styleListItems}>
+          <h3>{keg.name}</h3>
+        <p>Price per Pint: {keg.price}</p>
+        <p>Alcohol Content {keg.potency}% ABV</p>
+        <p>{keg.description}</p>
+        <p>Current Pints Remaining: {keg.quantity}</p>
+        <button className='button' onClick={()=> onServingPint(keg.id) }>Serve Pint</button>
+        </div>}
+
+        {keg.quantity <= 25 && keg.quantity > 10 &&
+          <div style = {styleQuarterLeft}>
+            <h3>{keg.name}</h3>
+            <p>Price per Pint: {keg.price}</p>
+            <p>Alcohol Content {keg.potency}% ABV</p>
+            <p>{keg.description}</p>
+            <p>Current Pints Remaining: {keg.quantity}</p>
+            <button className='button' onClick={()=> onServingPint(keg.id) }>Serve Pint</button>
+          </div>}
+
+        {keg.quantity <= 10 &&
+          <div style = {styleTenLeft}>
+          <h3>{keg.name}</h3>
+            <p>Price per Pint: {keg.price}</p>
+            <p>Alcohol Content {keg.potency}% ABV</p>
+            <p>{keg.description}</p>
+            <p>Current Pints Remaining: {keg.quantity}</p>
+            {keg.quantity > 0 &&
+              <button className='button' onClick={()=> onServingPint(keg.id) }>Serve Pint</button>}
+            {keg.quantity === 0 &&
+              <h1>Keg is Cashed. Waiting for some fresh brew.</h1> &&
+              <button className='kegButton' onClick={()=> onSwappingKeg(keg.id) }>Swap Keg</button>}
+        </div>}
     </>
   );
 }
@@ -45,15 +92,14 @@ KegDetails.propTypes = {
 
 export default KegDetails;
 
-{keg.quantity > 25 &&
-  <div style = {styleListItems}>
-    <h4>{keg.name}</h4>
-  </div>}
-{keg.quantity < 25 && keg.quantity > 11 &&
-  <div style = {styleQuarterLeft}>
-    <h4>{keg.name}</h4>
-</div>}
-{keg.quantity <= 10 &&
-<div style = {styleTenLeft}>
-  <h4>{keg.name}</h4>
-</div>}
+// <div style={styleListItems}>
+// <h3>{keg.name}</h3>
+// <p>Price per Pint: {keg.price}</p>
+// <p>Alcohol Content {keg.potency}% ABV</p>
+// <p>{keg.description}</p>
+// <p>Current Pints Remaining: {keg.quantity}</p>
+// {keg.quantity > 0 &&
+//   <button className='button' onClick={()=> onServingPint(keg.id) }>Serve Pint</button>}
+// {keg.quantity === 0 &&
+//   <h1>Keg is Cashed. Waiting for some fresh brew.</h1> &&
+//   <button className='kegButton' onClick={()=> onSwappingKeg(keg.id) }>Swap Keg</button>}
