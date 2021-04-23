f6c1843dd8a6edb926e8dae515a6d2d9205e0349
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-04-23 12:04:04 -0700

    Update Header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,46 +1,13 @@
 import React from "react";
-import styled from 'styled-components';
-import Button from './defaults/Button';
 
-const Navbar = styled.div`
-  padding: 5px 15px;
-  height: 45px;
-  background-color: rgb(89, 179, 239);
-`
-const StyledButton = styled(Button)`
-  padding: 8px;
-  margin-right: 5px;
-  border: 2px solid white;
-  background-color: transparent;
-  color: white;
-  border-radius: 5px;
-  outline: none;
-  &:hover {
-    background-color: white;
-    color: black;
-    cursor: pointer;
-    transition: .2s;
-  }
-`
-const StyledTitle = styled(Button)`
-  margin-left: 35%;
-  background-color: transparent;
-  border-style: none;
-  color: white;
-  font-size: 30px;
-  outline: none;
-  &:hover {
-    cursor: pointer;
-    transition: .2s;
-  }
-`
+function Header(){
+  return (
+    <div className='HeaderTitle'>
+      <h1>Brew-Ha House</h1>
+    </div>
+  );
+}
 
-const Header = () => (
-  <Navbar>
-    <StyledButton text={"Home"} />
-    <StyledButton text={"Kegs"} />
-    <StyledTitle text={"Available Kegs"} />
-  </Navbar>
-)
+
 
 export default Header;
