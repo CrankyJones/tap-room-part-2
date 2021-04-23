55ad81bc7ed5d5beadfffb1c18bc62ab59a6caf0
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-04-23 10:28:49 -0700

    Update KegControl.js

---
+++ src/components/KegControl.js
@@ -0,0 +1,65 @@
+import React from "react";
+import NewKegForm from "./NewKegForm";
+import KegDetails from "./KegDetails";
+import KegList from "./KegList";
+
+class KegControl extends React.Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        formVisibleOnPage: false,
+        masterKegList: [],
+        selectedKeg: null
+      };
+  }
+
+  handleClick = () => {
+    if (this.state.selectedKeg != null) {
+      this.setState({
+        formVisibleOnPage: false,
+        selectedKeg: null
+      });
+    } else {
+      this.setState(prevState => ({
+        formVisibleOnPage = !prevState.formVisibleOnPage
+      }));
+    }
+  }
+
+  handleChangingSelectedKeg = (id) => {
+    const selectedKeg = this.state.masterKegList.filter(product => product.id === id)[0];
+    this.setState({selectedKeg: selectedKeg});
+  }
+
+  handleAddingNewKegToList(newKeg) {
+    const newMasterKegList = masterKegList.concat(newKeg);
+    this.setState({masterKegList: newMasterKegList,
+                    formVisibleOnPage: false})
+  }
+
+
+  render(){
+    let currentlyVisibleState = null;
+    let buttonText = null;
+
+    if (this.state.selectedKeg != null) {
+      currentlyVisibleState = <KegDetails keg = {this.state.selectedKeg}/>
+      buttonText = "Return to Keg List";
+    } else if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewKegForm onNewKegCreation = {handleAddingNewKegToList}/>
+      buttonText = "Return to Keg List";
+    } else {
+      currentlyVisibleState = <KegList kegList = {this.state.masterKegList} 
+                                      onKegSelection = {this.handleChangingSelectedKeg}/>;
+      buttonText = "Add Keg"
+    }
+    return(
+      <>
+      {currentlyVisibleState}
+      <button onCLick = {this.handleClick}>{buttonText}</button>
+      </>
+    );
+  }
+}
+
+export default KegControl;
---
+++ src/components/KegDetails.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function KegDetails(props){
+
+  return (
+    <>
+    </>
+  );
+}
+
+KegDetails.propTypes = {
+
+
+};
+
+export default KegDetails
