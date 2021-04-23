6ed06c51cbb7c3f85ce5700f43f0c5d8fe7a779a
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-04-23 13:28:19 -0700

    Update Header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,23 @@
 import React from "react";
 
+
+
+
+
+
+
 function Header(){
+  const styleTitleObject = {
+    backgroundColor: "Black",
+    color: "white",    
+    textAlign: 'center',
+    padding: '20px',
+  }
   return (
-    <div className='HeaderTitle'>
-      <h1>Brew-Ha House</h1>
-    </div>
+    <>
+      <h1 style={styleTitleObject}>Brew-Ha House</h1>
+      <h4>Check out our current on-tap beverages! </h4>
+    </>
   );
 }
 
