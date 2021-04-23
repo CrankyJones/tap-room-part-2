6aa769540c8ef39c0a0956c72d2530ea69e9b98f
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-04-23 08:54:38 -0700

    Update file structure and Keg.js

---
+++ src/components/Keg.js
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Keg(props) {
+  return(
+    <>
+    <div onClick ={() => props.whenKegClicked(props.id)}>
+      <h4>{props.name}</h4>
+      <p>{props.price}</p>
+      <p>{props.description}</p>
+      <p>{props.potency}</p>
+      <p>Pints Remaining: {props.quantity}</p>
+    </div>
+    <hr/>
+    </>
+  );
+}
+
+Keg.porptypes = {
+  name: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  potency: PropTypes.string.isRequired,
+  quantity:  PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  whenKegClicked: PropTypes.func
+}
+
+export default Product;
