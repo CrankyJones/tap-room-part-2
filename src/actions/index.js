501af12c90ab8c0ad6db2b313a4e45cdf1b66d05
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-04-30 10:59:27 -0700

    Passing actions/index tests

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import * as c from './../actions/ActionTypes';
+
+export const addKeg = (keg) => {
+  const { name, price, description, potency, quantity, id } = keg;
+  return { 
+    type: c.ADD_KEG,
+    name: name,
+    price: price,
+    description: description,
+    potency: potency,
+    quantity: quantity,
+    id: id
+  }
+}
+
+export const toggleForm = () => ({
+  type: c.TOGGLE_FORM
+});
